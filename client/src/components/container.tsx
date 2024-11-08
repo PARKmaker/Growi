eb397ae4a54b6e7d24f2acf5c535b97bd7bdc581
PARKmaker <tkdgus8903@gmail.com>
2024-11-09 00:26:16 +0900

    Style: 가로폭 최소 크기 설정

--- client/src/components/container.tsx
+++ client/src/components/container.tsx
@@ -4,5 +4,7 @@ type ContainerProps = {
   children: ReactNode;
 };
 export default function Container({ children }: ContainerProps) {
-  return <div className="relative my-4 px-4 md:container md:my-8">{children}</div>;
+  return (
+    <div className="relative mx-auto my-4 min-w-[320px] px-4 md:container md:my-8">{children}</div>
+  );
 }
