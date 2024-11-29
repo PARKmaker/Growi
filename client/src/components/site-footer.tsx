5e0eaa8671e075de30b13851110786d48039882a
PARKmaker <tkdgus8903@gmail.com>
2024-11-29 21:36:02 +0900

    Fix: 빌드 오류 해결

--- client/src/components/site-footer.tsx
+++ client/src/components/site-footer.tsx
@@ -5,16 +5,14 @@ export default function SiteFooter() {
     <footer className="py-6 md:px-8 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
-          Built by{' '}
-          <a
-            href={siteConfig.links.twitter}
-            target="_blank"
-            rel="noreferrer"
-            className="font-medium underline underline-offset-4"
-          >
-            복리계산기
-          </a>
-          . The source code is available on{' '}
+          Built by {/*<a*/}
+          {/*  href={siteConfig.links.twitter}*/}
+          {/*  target="_blank"*/}
+          {/*  rel="noreferrer"*/}
+          {/*  className="font-medium underline underline-offset-4"*/}
+          {/*>*/}
+          {/*  복리계산기*/}
+          {/*</a>*/}. The source code is available on{' '}
           <a
             href={siteConfig.links.github}
             target="_blank"
